8a531d257b4ab929ebfc5937cef22b2572f4e83c
yy865486385 <865486385@qq.com>
2020-10-30 08:48:19 +0800

    init

---
+++ SimpleFactoryPattern/src/main/java/com/demo/SimpleFactoryPattern.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+/**
+ * 简单工程模式
+ *
+ * @author Yu_Yang
+ * @date 2020-10-29
+ */
+public class SimpleFactoryPattern {
+    public static void main(String[] args) {
+        System.out.println("简单工程模式");
+    }
+}
---
+++ StrategyPattern/src/main/java/com/demo/StrategyPattern.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+/**
+ * 策略模式
+ *
+ * @author Yu_Yang
+ * @date 2020-10-29
+ */
+public class StrategyPattern {
+    public static void main(String[] args) {
+        System.out.println("策略模式");
+    }
+}
